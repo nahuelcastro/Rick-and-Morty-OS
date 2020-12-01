57d8e2728b205d379f3dc4b3f57cfa219ea34d1c
Juan Manuel Torsello <jtorsello@dc.uba.ar>
2020-12-01 17:55:02 -0300

    mmu_init_task_dir mejor

--- src/mmu.c
+++ src/mmu.c
@@ -141,6 +141,50 @@ paddr_t mmu_unmap_page(uint32_t cr3, vaddr_t virt){
 
 }
 
+
+
+paddr_t mmu_init_task_dir(paddr_t phy_start, paddr_t code_start, size_t pages){
+
+  uint32_t new_cr3 = mmu_next_free_kernel_page(); //cr3: 0x000000105000
+  uint32_t cr3 = 0x25000;
+
+
+  paddr_t tasks_memory = TASK_CODE_VIRTUAL;
+  //paddr_t phy_address = phy_start;
+
+  paddr_t memory_kernel = 0;
+
+  // Mapeo kernel
+  for (size_t i = 0; i < 1024; i++){
+    mmu_map_page(new_cr3,memory_kernel,memory_kernel,2);
+    memory_kernel += 4096;
+  }
+
+  // mapea espacio de tarea en el cr3 de la tarea
+  for (size_t i = 0; i < pages; i++){
+    mmu_map_page(new_cr3, tasks_memory, phy_start, 6);  //era 2 pero necesitamos privilegios de usuario sino rompe cuando entramos en tarea
+    tasks_memory += 4096; // 4096 = 4kb = tamaño pagina
+    phy_start += 4096;  
+  }
+  
+  lcr3(new_cr3);
+
+  char* ptr_code_page = (char*) code_start;
+  char* ptr_virt_page = (char*) TASK_CODE_VIRTUAL;
+  
+  for (size_t i = 0; i < 16384; i++){
+    ptr_virt_page[i] = ptr_code_page[i];
+  }
+
+  lcr3(cr3);
+
+  return new_cr3;
+}
+
+
+
+
+/*
 paddr_t mmu_init_task_dir(paddr_t phy_start, paddr_t code_start, size_t pages){
   // Está en ppio que haga lo que dice enunciado 5 es decir:
   //paddr_t task_page = mmu_next_free_task_page();
@@ -194,13 +238,6 @@ paddr_t mmu_init_task_dir(paddr_t phy_start, paddr_t code_start, size_t pages){
   }
 
 
-  // for (size_t i = 0; i < 16384; i++)
-  // {
-  //   *ptr_virt_page = *ptr_code_page;
-  //   ptr_code_page++;
-  //   ptr_virt_page++;
-  // }
-
   for (size_t i = 0; i < pages; i++){
     mmu_unmap_page(cr3, tasks_memory);
     tasks_memory += 4096; // 4096 = 4kb = tamaño pagina
@@ -211,11 +248,4 @@ paddr_t mmu_init_task_dir(paddr_t phy_start, paddr_t code_start, size_t pages){
 
   return new_cr3;
 }
-
-/*
-
-info gdt
-
-
-
 */
